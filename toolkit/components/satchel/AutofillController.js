45796729c4a0bb2ced0ec0ca425f4f8fb0b4d267
Brian Nicholson <bnicholson@mozilla.com>
2014-05-22 10:47:16 -0700

    smaug-wip

--- toolkit/components/satchel/AutofillController.js
+++ toolkit/components/satchel/AutofillController.js
@@ -5,8 +5,10 @@
 
 const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
 
-Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://gre/modules/AutofillContract.jsm");
+Cu.import("resource://gre/modules/Promise.jsm");
+Cu.import("resource://gre/modules/Task.jsm");
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
 XPCOMUtils.defineLazyServiceGetter(this, "AutofillUIGlue",
                                    "@mozilla.org/autofill-ui-glue;1",
@@ -15,37 +17,38 @@ XPCOMUtils.defineLazyServiceGetter(this, "AutofillUIGlue",
 XPCOMUtils.defineLazyModuleGetter(this, "AutofillProvider",
                                   "resource://gre/modules/AutofillProvider.jsm");
 
-/*
- * requestAutocomplete implementation
- * See: http://www.chromium.org/developers/using-requestautocomplete#h.mbcntk221rc4
- *      http://www.whatwg.org/specs/web-apps/current-work/multipage/association-of-controls-and-forms.html#attr-fe-autocomplete
- */
 function AutofillController() {}
 
 AutofillController.prototype = {
-  _form: null,
-  _win: null,
-  _isActive: false,
+  _activeWindows: new WeakMap(),
 
   _parseAutocomplete: function (attr) {
-    function attrGenerator(attr) {
+    function* attrGenerator(attr) {
       attr = attr.toLowerCase();
       let regex = /\S+/g;
       let match;
       while (match = regex.exec(attr)) {
         yield match[0];
       }
-      yield null;
+    }
+
+    let gen = attrGenerator(attr);
+
+    function getToken() {
+      let result = gen.next();
+      if (result.done) {
+        throw "Unexpected end of autocomplete attributes";
+      }
+      return result.value;
     }
 
     // Use shipping data by default.
     let type = "shipping";
 
-    let gen = attrGenerator(attr);
-    let token = gen.next();
+    let token = getToken();
     if (token === "billing" || token === "shipping") {
       type = token;
-      token = gen.next();
+      token = getToken();
     }
 
     let key;
@@ -59,7 +62,7 @@ AutofillController.prototype = {
     }
 
     // Abort if there are extra attributes we're not expecting.
-    if (gen.next() != null) {
+    if (!gen.next().done) {
       throw "Unexpected autocomplete attribute";
     }
 
@@ -69,7 +72,7 @@ AutofillController.prototype = {
     };
   },
 
-  _submit: function (data) {
+  _submit: function (form, data) {
     function getEntryFromGuid(type, guid) {
       for (let entry of type.entries) {
         if (entry.guid === guid) {
@@ -104,7 +107,8 @@ AutofillController.prototype = {
       AutofillProvider.saveSelections(guids);
 
       // BRN: handle select fields restrictions
-      let els = this._form.querySelectorAll("input,select");
+      let win = form.ownerDocument.defaultView;
+      let els = form.querySelectorAll("input,select");
       for (let i = 0; i < els.length; ++i) {
         let el = els[i];
         let attr = el.getAttribute("autocomplete");
@@ -117,6 +121,7 @@ AutofillController.prototype = {
         try {
           field = this._parseAutocomplete(attr);
         } catch (e) {
+          // BRN: does this need to be logged?
           continue;
         }
 
@@ -147,54 +152,57 @@ AutofillController.prototype = {
         }
 
         el.value = value;
-        let evt = new this._win.Event("change", { bubbles: true });
+        let evt = new win.Event("change", { bubbles: true });
         el.dispatchEvent(evt);
       }
 
       // Fire event on the form to indicate success.
-      let evt = new this._win.Event("autocomplete", { bubbles: true });
-      this._form.dispatchEvent(evt);
-
-      this._finish();
+      let evt = new win.Event("autocomplete", { bubbles: true });
+      form.dispatchEvent(evt);
     }.bind(this));
   },
 
-  _finish: function () {
-    this._form = null;
-    this._win = null;
-    this._isActive = false;
-  },
+  requestAutocomplete: function (form) {
+    let win = form.ownerDocument.defaultView;
 
-  _cancel: function () {
-    let evt = new this._win.AutocompleteErrorEvent("autocompleteerror", { bubbles: true, reason: "cancel" });
-    this._form.dispatchEvent(evt);
-    this._finish();
-  },
+    function dispatchError(reason) {
+      let evt = new win.AutocompleteErrorEvent("autocompleteerror", { bubbles: true, reason: reason });
+      form.dispatchEvent(evt);
+    }
 
-  requestAutocomplete: function (aForm) {
-    let win = aForm.ownerDocument.defaultView;
+    function dispatchDisabled(message) {
+      Cu.reportError("requestAutocomplete disabled: " + message);
+      Task.spawn(function () {
+        dispatchError("disabled");
+      });
+    }
 
-    if (!win) {
-      Cu.reportError("requestAutocomplete called on element with no window");
+    let dwu = win.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+                 .getInterface(Components.interfaces.nsIDOMWindowUtils);
+
+    if (!dwu.isHandlingUserInput) {
+      dispatchDisabled("requestAutocomplete must be initiated by a trusted input event");
       return;
     }
 
-    if (this._isActive || aForm.autocomplete === "off") {
-      let evt = new win.AutocompleteErrorEvent("autocompleteerror", { bubbles: true, reason: "disabled" });
-      aForm.dispatchEvent(evt);
+    if (form.autocomplete === "off") {
+      dispatchDisabled("form autocomplete attribute is off");
       return;
     }
 
-    // isActive flag is set as specified by http://wiki.whatwg.org/wiki/RequestAutocomplete
-    // BRN: how will we handle isActive with multiple tabs?
-    this._isActive = true;
-    this._win = win;
-    this._form = aForm;
+    if (this._activeWindows.has(win)) {
+      dispatchDisabled("another dialog is already active for this window");
+      return;
+    }
 
-    AutofillUIGlue.onRequestAutocomplete({
-      submit: this._submit.bind(this),
-      cancel: this._cancel.bind(this)
-    });
+    this._activeWindows.set(win, true);
+
+    let submit = (data) => this._submit(form, data);
+    let cancel = () => dispatchError("cancel");
+    let done = () => this._activeWindows.delete(win);
+    new Promise((resolve, reject) => AutofillUIGlue.onRequestAutocomplete({ submit: resolve, cancel: reject }))
+      .then(submit, cancel)
+      .then(done);
   },
 
   classID: Components.ID("{ed9c2c3c-3f86-4ae5-8e31-10f71b0f19e6}"),
